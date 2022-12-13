b738fc43c68bc055d76e320481eae66092f0b8c3
mattb103190 <matt@bryllyant.com>
2022-12-13 16:46:11 -0600

    WIP

--- mood/src/app/app.module.ts
+++ mood/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
@@ -13,6 +14,7 @@ import { SharedModule } from '@shared';
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     CoreModule,
     SharedModule,
